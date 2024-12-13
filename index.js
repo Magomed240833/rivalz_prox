f4ee5f0edbad70f5b191e77dd00b952bf36b817b
Magomed240833 <bmaga2408@gmail.com>
2024-12-13 04:09:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 const pty = require("node-pty");
-
 const os = require("os");
 
-
+// Чтение переменных окружения
 const walletAddress = process.env.WALLET_ADDRESS;
 const cpuCores = process.env.CPU_CORES;
 const ram = process.env.RAM;
 const diskSize = process.env.DISK_SIZE;
 const diskSelection = process.env.DISK_SELECTION || "";
 
+// Прокси параметры
+const proxyIP = process.env.PROXY_IP;
+const proxyPort = process.env.PROXY_PORT;
+const proxyType = process.env.PROXY_TYPE || 'http';
+const proxyUser = process.env.PROXY_USER;
+const proxyPass = process.env.PROXY_PASS;
+
 if (!walletAddress || !cpuCores || !ram || !diskSize) {
-  console.error(
-    "Error : WALLET_ADDRESS, CPU_CORES, RAM, DISK_SIZE"
-  );
+  console.error("Error: WALLET_ADDRESS, CPU_CORES, RAM, DISK_SIZE are required.");
   process.exit(1);
 }
 
+// Проверка наличия прокси данных
+if (proxyIP && proxyPort) {
+  console.log(Using proxy: ${proxyType}://${proxyUser}:${proxyPass}@${proxyIP}:${proxyPort});
+}
+
+// Инициализация pty для запуска приложения
 const ptyProcess = pty.spawn("rivalz", ["run"], {
   name: "xterm-color",
   cols: 80,
@@ -24,20 +34,13 @@ const ptyProcess = pty.spawn("rivalz", ["run"], {
   env: process.env,
 });
 
+// Функция для отправки данных в процессе
 function sendInput(data) {
   if (data.includes("Enter wallet address (EVM):")) {
     ptyProcess.write(walletAddress + "\r");
-  } else if (
-    data.includes(
-      "Enter number of CPU cores you want to allow the client to use:"
-    )
-  ) {
+  } else if (data.includes("Enter number of CPU cores you want to allow the client to use:")) {
     ptyProcess.write(cpuCores);
-  } else if (
-    data.includes(
-      "Enter amount of RAM (GB) you want to allow the client to use:"
-    )
-  ) {
+  } else if (data.includes("Enter amount of RAM (GB) you want to allow the client to use:")) {
     ptyProcess.write(ram);
   } else if (data.includes("Select drive you want to use:")) {
     if (diskSelection) {
@@ -48,15 +51,13 @@ function sendInput(data) {
   }
 }
 
-// Écoute les données du processus
+// Слушаем данные от процесса
 ptyProcess.on("data", function (data) {
   process.stdout.write(data);
   sendInput(data);
 });
 
-// Gère la fin du processus
+// Обрабатываем завершение процесса
 ptyProcess.on("exit", function (code, signal) {
-  console.log(
-    `The process ended with the code ${code} and the signal ${signal}.`
-  );
+  console.log(The process ended with the code ${code} and the signal ${signal}.);
 });
