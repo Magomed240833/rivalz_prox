a5b33c89e8147f1105b2575d4e0c60dbc52b32b2
Magomed240833 <bmaga2408@gmail.com>
2024-12-13 10:29:26 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# Запуск redsocks
+redsocks -c /etc/redsocks.conf &
+echo "Redsocks запущен."
+
+# Дайте redsocks время на инициализацию
+sleep 5
+
+# Запуск ноды Rivalz
 node index.js
 
+# Удерживаем контейнер в активном состоянии
 tail -f /dev/null
